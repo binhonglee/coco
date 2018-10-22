24616ca378b1f9c8d8798ad8e32b70015fdf656d
Samuel Roy <sam@pricemetry.com>
2018-10-22 22:32:53 +0200

    Use Nim APIs instead of shell command #11

--- tests/report/generate_coverage.nim
+++ tests/report/generate_coverage.nim
@@ -7,23 +7,25 @@ suite "Generate code coverage report":
         var coverage = "tests_coverage"
         var nimcache = "tests_nimcache_report"
         var basename = "foo"
-        var filename = &"{basename}.nim"
+        var filename = fmt"{basename}.nim"
         var default_cache_folder = get_cache_folder(filename, nimcache, 0)
-        var base_filename = &"{default_cache_folder}/coco_{basename}.c"
+        var base_filename = fmt"{default_cache_folder}/coco_{basename}.c"
     teardown:
-        discard execShellCmd(&"rm -rf {fileinfo} {coverage} {nimcache}")
+        removeDir(coverage)
+        removeDir(nimcache)
+        removeFile(fileinfo)
 
     test "Generate line code coverage only from test file foo.nim":
 
-        discard coverage(target = &"tests/{filename}", nimcache = nimcache, report_source = fileinfo, report_path = coverage)
+        discard coverage(target = fmt"tests/{filename}", nimcache = nimcache, report_source = fileinfo, report_path = coverage)
         
         check:
-            execShellCmd(&"ls {coverage}") == 0
-            execShellCmd(&"ls {coverage}/index-sort-b.html") == 1 # should not generate branch coverage
+            existsDir(coverage) == true
+            existsFile(fmt"{coverage}/index-sort-b.html") == false # should not generate branch coverage
     
     test "Generate code coverage in branch mode only from test file foo.nim":
 
-        discard coverage(target = &"tests/{filename}", nimcache = nimcache, report_source = fileinfo, report_path = coverage, branch = true, verbose = true, compiler = "--hints:off")
+        discard coverage(target = fmt"tests/{filename}", nimcache = nimcache, report_source = fileinfo, report_path = coverage, branch = true, verbose = true, compiler = "--hints:off")
         
         check:
-            execShellCmd(&"ls {coverage}/index-sort-b.html") == 0
+            existsFile(fmt"{coverage}/index-sort-b.html") == true
